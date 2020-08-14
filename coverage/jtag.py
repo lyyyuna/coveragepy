f5fd7be890d121382d333e495ca48b087d8061cf
lyyyuna <lyyyuna@outlook.com>
2020-08-14 17:48:34 +0800

    remove bottle

--- coverage/jtag.py
+++ coverage/jtag.py
@@ -1,6 +1,7 @@
 import threading
 import time
-import bottle
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from urllib.parse import urlparse, parse_qs
 from coverage import Coverage, CoverageData
 
 
@@ -11,11 +12,20 @@ class Jtag(threading.Thread):
         self.coverage = coverage
 
     def run(self):
-        app = bottle.Bottle()
-
-        @app.route('/')
-        def hello():
-            cov: CoverageData = self.coverage.get_data()
-            return str(cov.lines('/Users/lyyyuna/gitup/test/test.py'))
-
-        app.run(host='localhost', port=8082, quiet=True)
+        cov = self.coverage
+        class MyHandler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                path = urlparse(self.path).path
+                query = urlparse(self.path).query
+                if path == '/v1/lines':
+                    name = parse_qs(query).get('file')
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/html")
+                    self.end_headers()
+                    data = cov.get_data()
+                    self.wfile.write(str(data.lines('/Users/lyyyuna/gitup/test/test.py') + name).encode())
+                else:
+                    self.send_error(404)
+        server_address = ('', 8000)
+        httpd = ThreadingHTTPServer(server_address, MyHandler)
+        httpd.serve_forever()
